86c467e55db20c4484a7657b9144314b19cf8a94
Gremlin4544 <alve@mckethen.com>
2020-02-14 11:38:58 -0500

    added styles to logo and links

--- client/src/App.js
+++ client/src/App.js
@@ -9,16 +9,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.fifa.com/womensworldcup/teams/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          View 2019 Teams
+        </a>
+        <br />
+        <a
+          className="App-link2"
+          href="https://www.fifa.com/womensworldcup/awards/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View 2019 Awards
         </a>
       </header>
       <Players />
