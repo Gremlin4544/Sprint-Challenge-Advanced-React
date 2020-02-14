4003af33d2395e1b3dc483b9e79fc8ff8c7c9e51
Gremlin4544 <alve@mckethen.com>
2020-02-14 11:45:31 -0500

    imported dark mode to app.js and added styling, button works

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,17 @@ import logo from '../src/img/logo.png';
 import './App.css';
 
 import Players from "../src/components/Players";
+import useDarkMode from "../src/hooks/useDarkMode";
 
 function App() {
+
+  const [darkMode, setDarkMode] = useDarkMode(false);
+
+  const toggleDarkMode = event => {
+    event.preventDefault();
+    setDarkMode(!darkMode);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -27,6 +36,7 @@ function App() {
         >
           View 2019 Awards
         </a>
+        <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
       </header>
       <Players />
     </div>
